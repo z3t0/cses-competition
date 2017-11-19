4dfa21f6e46e6df6598b56df7d0951111e7bd587
Rafi Khan <zetoslab@gmail.com>
2017-11-18 22:35:27 -0500

    building done

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Modules
 const express = require('express')
 const app = express()
 const fs = require('fs')
@@ -5,8 +6,8 @@ const bodyParser = require('body-parser');
 const multer = require('multer'); // v1.0.5
 const upload = multer(); // for parsing multipart/form-data
 
-const file_name = "data.json"
 
+const file_name = "data.json"
 const DEBUG = true
 
 app.use(bodyParser.json()); // for parsing application/json
@@ -14,20 +15,23 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 
 
 debug = (msg) => {
-    if (debug)
+    if (DEBUG)
     {
 	console.log(msg)
-	// TODO: send errors back maybe...
     }
 }
 
+
+// Load data
 let load = (path) => {
     let data
 
+    // Check if the file exists
     if (fs.existsSync(path)) {
 	data = fs.readFileSync(path)
 	data = JSON.parse(data)
     } else {
+	// Create an Empty file
 	data = {}
 	save(data, path)
     }
@@ -35,7 +39,9 @@ let load = (path) => {
     return data
 }
 
+// Save data
 let save = (data, path) => {
+    // Write the data to file
     fs.writeFile(path, JSON.stringify(data), 'utf8', (err) => {
 	if (err) {
 	    debug(err)
@@ -46,28 +52,35 @@ let save = (data, path) => {
     })
 }
 
+// Load data on startup
 let data = load(file_name)
 
+// Serve the front-end
 app.use(express.static('public'))
 
+// Send the database
 app.get('/api/load/', (req, res) => {
     res.send(JSON.stringify(data))
 })
 
+// Save the modified database
 app.post('/api/save/', upload.array(), (req, res, next) => {
+
     try {
+	// Parse
 	data = req.body
 	save(data, file_name)
     } catch (e) {
+	// Let the front-end know there was an issue
 	res.send("failed")
 	debug(e)
 	return
     }
 
+    // Otherwise let it know it was successful
     res.send("successful")
 })
 
-
-
+// Listen on port
 app.listen(3000, () => debug("Listening on port 3000"))
 
